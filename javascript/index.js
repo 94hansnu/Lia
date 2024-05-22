be70285af75adec77ba5e64bcdce15e52f9ea297
Jafar Hussein <Jafarhussein6@gmail.com>
2024-05-22 18:46:07 +0200

    filtrerade bort svärord

--- javascript/index.js
+++ javascript/index.js
@@ -1,10 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
     const startQuizBtn = document.getElementById('startQuiz');
+    const forbiddenWords = [
+        "Bög", "Fitta", "Kuk", "Fuck", "Bitch", "Shit", "Whore", "Hora",
+        "Dick", "Pussy", "Nigga", "Röv", "Skit", "Helvete", "Jävla", "Knulla",
+        "Pung", "Idiot", "Mongo", "Djävla", "Damp", "Cunt", "Wanker", "Neger",
+        "Dachri", "Horunge", "Kuksugare", "Gahba", "Kahba", "Kahbe", "Nigger",
+        "Jävel"
+    ];
     startQuizBtn.addEventListener('click', () => {
         const userName = document.getElementById('userName').value;
         if (userName.trim() !== '') {
-            localStorage.setItem('userName', userName);
-            window.location.href = 'quiz.html';
+            const lowerCaseUserName = userName.toLowerCase();
+            const containsForbiddenWord = forbiddenWords.some(word => lowerCaseUserName.includes(word.toLowerCase()));
+
+            if (containsForbiddenWord) {
+                alert('Användarnamnet innehåller olämpliga ord. Vänligen ange ett annat namn.');
+            } else {
+                localStorage.setItem('userName', userName);
+                window.location.href = 'quiz.html';
+            }
         } else {
             alert('Ange ditt namn för att fortsätta.');
         }
